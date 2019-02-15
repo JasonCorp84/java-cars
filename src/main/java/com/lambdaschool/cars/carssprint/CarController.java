aaf7b66a47aa7348f50aa5065c36bc097677248d
JasonCorp84 <csababalogh84@gmail.com>
2019-02-15 10:26:59 -0800

    CRUD ops done

--- src/main/java/com/lambdaschool/cars/carssprint/CarController.java
+++ src/main/java/com/lambdaschool/cars/carssprint/CarController.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.Comparator;
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,12 +22,30 @@ public class CarController {
         this.rt = rt;
     }
     @GetMapping("cars/id/{id}")
-    public List<Car> catById(@PathVariable Long id) {
-        return carRepos.findById(id).stream().collect(Collectors.toList());
+    public List carById(@PathVariable Long id) {
+        Car car = carRepos.findById(id).orElseThrow();
+        List cars = new ArrayList();
+        cars.add(car);
+        return cars;
+    }
+    @GetMapping("/cars/year/{year}")
+    public List<Car> carByYear(@PathVariable int year) {
+        return carRepos.findAll().stream().filter(car -> car.getYear() == year).collect(Collectors.toList());
+    }
+    @GetMapping("/cars/brand/{brand}")
+    public List<Car> carByBrand(@PathVariable String  brand) {
+        return carRepos.findAll().stream().filter(car -> car.getBrand().toLowerCase().equals(brand.toLowerCase())).collect(Collectors.toList());
     }
 
     @PostMapping("/cars/upload")
     public List<Car> newCountry(@RequestBody List<Car> newCountries){
         return carRepos.saveAll(newCountries);
+    }
+
+    @DeleteMapping("/cars/delete/{id}")
+    public Car deleteById(@PathVariable Long id) {
+        Car car = carRepos.findById(id).orElseThrow();
+        carRepos.delete(car);
+        return car;
     }
 }
