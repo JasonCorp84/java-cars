71bb4ac820f2da298e316000801dcdb848561d89
JasonCorp84 <csababalogh84@gmail.com>
2019-02-15 09:21:40 -0800

    Car controller: - cars from json file being uplodaed

---
+++ src/main/java/com/lambdaschool/cars/carssprint/CarController.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.cars.carssprint;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Comparator;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+
+public class CarController {
+    private final CarRepo carRepos;
+    private final RabbitTemplate rt;
+
+    public CarController(CarRepo carRepos, RabbitTemplate rt) {
+        this.carRepos = carRepos;
+        this.rt = rt;
+    }
+    @PostMapping("/cars/upload")
+    public List<Car> newCountry(@RequestBody List<Car> newCountries){
+        return carRepos.saveAll(newCountries);
+    }
+}
