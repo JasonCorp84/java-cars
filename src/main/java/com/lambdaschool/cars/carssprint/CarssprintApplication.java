7b346d35666a35ced8b51f65328e9d6c56ae58e3
JasonCorp84 <csababalogh84@gmail.com>
2019-02-15 08:55:52 -0800

    project initiated

---
+++ src/main/java/com/lambdaschool/cars/carssprint/CarssprintApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.cars.carssprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarssprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarssprintApplication.class, args);
+    }
+
+
+}
+
